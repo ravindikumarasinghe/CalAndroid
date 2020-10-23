2f3fbd6a6c8427f46c47485ec6433d009d1289df
Ravindi Kumarasinghe <57246252+ravindikumarasinghe@users.noreply.github.com>
2020-10-23 11:58:41 +0530

    Initialize textview in MainActivity.java

--- app/src/main/java/com/example/calandroid/MainActivity.java
+++ app/src/main/java/com/example/calandroid/MainActivity.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button b1,b2,b3,b4;
+    TextView rs;
 
 
     @Override
