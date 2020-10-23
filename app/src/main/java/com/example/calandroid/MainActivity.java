356c1b854f3289e3bc40b0e3c31041c91f954a89
Ravindi Kumarasinghe <57246252+ravindikumarasinghe@users.noreply.github.com>
2020-10-23 11:57:25 +0530

    Initialize buttons in MainActivity.java

--- app/src/main/java/com/example/calandroid/MainActivity.java
+++ app/src/main/java/com/example/calandroid/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.calandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button b1,b2,b3,b4;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
